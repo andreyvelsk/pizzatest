2d90be923f01aa8e2c090a589d1663694e6f9224
andrey <andrey_velsk@mail.ru>
2018-10-29 17:44:40 +0300

    Анимация переходов router

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
 
-    <router-view></router-view>
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
 
   </div>
       <!-- /#app -->
